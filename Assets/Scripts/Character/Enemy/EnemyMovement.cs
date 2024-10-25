b4d57e83793ee0a2bf10efa52dba8838571b355c
森本匠 <>
2024-10-25 12:20:19 +0900

    Update MainGame

--- Assets/Scripts/Character/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Character/Enemy/EnemyMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    private EnemyMovement[] _enemyMove;       //メニューなどのボタンを格納するための配列変数
+
+    private enum _movement
+    {
+        Bat,
+        Goat
+    }
+    [SerializeField] _movement EnemyMove;　　　　//プルダウン化
 
     private GameObject _god;
     private Vector3 _godPos;
@@ -21,6 +29,11 @@ public class EnemyMovement : MonoBehaviour
     Rigidbody2D rb;
     Animator _animator;
 
+    // a=円運動の大きさ, b=円運動の速さ
+    public float CircleAx, CircleAy, CircleBx, CircleBy;
+    // タイマー
+    float t;
+
     private void Start()
     {
         _god = GameObject.FindGameObjectWithTag("God");
@@ -29,7 +42,22 @@ public class EnemyMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         Dead = Enemy.GetComponent<IsDamaged>();
     }
+
     private void Update()
+    {
+        // コウモリ
+        if(EnemyMove == _movement.Bat)
+        {
+            DevilBat();
+        }
+        // ヤギ
+        else if (EnemyMove == _movement.Goat)
+        {
+            DevilGoat();
+        }
+    }
+
+    private void DevilBat()
     {
         // 仮死状態ではないなら処理
         if (Dead.IsDead == false)
@@ -40,6 +68,35 @@ public class EnemyMovement : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, _godPos, statusdata.SPEED * Time.deltaTime);
             //神と敵キャラのX軸の位置関係を取得する
             _diff.x = _godPos.x - this.transform.position.x;
+
+            if (_diff.x > 0)
+            {
+                // Godが敵キャラの右側にいる時右側を向く
+                _vector = new Vector3(0, -180, 0);
+                this.transform.eulerAngles = _vector;
+            }
+            if (_diff.x < 0)
+            {
+                // Godが敵キャラの左側にいる時左側を向く
+                _vector = new Vector3(0, 0, 0);
+                this.transform.eulerAngles = _vector;
+            }
+        }
+    }
+
+    private void DevilGoat()
+    {
+        // 仮死状態ではないなら処理
+        if (Dead.IsDead == false)
+        {
+            //神の現在位置を取得
+            _godPos = _god.transform.position;
+            t += Time.deltaTime;
+            float x = CircleAx * Mathf.Cos(t * CircleBx);
+            float y = CircleAy * Mathf.Sin(t * CircleBy);
+            transform.position
+                = new Vector3(_godPos.x + x, _godPos.y + y, 0.0f);
+            transform.rotation = Quaternion.identity;
 
             if (_diff.x > 0)
             {
